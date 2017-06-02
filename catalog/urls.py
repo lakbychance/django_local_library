f5f9ffa177f97dc1e84890dc273addc7948e01e4
Lakshya Thakur <GREYORANGE\lakshya.t@ggn000434.greyorange.com>
2017-06-02 13:01:14 +0530

    First version of application moved into github

---
+++ catalog/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^books/$', views.BookListView.as_view(), name='books'),
+	url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+	url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+	url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+	url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+	url(r'^borrowed/$', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+	url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
